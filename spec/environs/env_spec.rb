5290b637d99bf6529e485a0bba26a834a1be8517
Colin Rymer <colin.rymer@gmail.com>
2013-07-02 11:56:22 -0400

    fix 1.8 compatiblity

--- spec/environs/env_spec.rb
+++ spec/environs/env_spec.rb
@@ -7,11 +7,16 @@ describe Env, "#foo" do
     Env.foo.should eq 'bar'
   end
 
+  it "converts method name to SCREAMING_SNAKE_CASE for lookup" do
+    ENV['FOO_BAR_BAZ'] = 'bar'
+    Env.foo_bar_baz.should eq 'bar'
+  end
+
   describe "default behavior" do
 
     it "returns an error if no corresponding ENV variable is set" do
-      ENV['FOO'] = nil
-      expect { Env.foo }.to raise_error
+      ENV['FOO_NO_VALUE'] = nil
+      expect { Env.foo_no_value }.to raise_error
     end
 
   end
@@ -19,8 +24,8 @@ describe Env, "#foo" do
   describe "'allow_nil: true' is passed along" do
 
     it "returns nil if no corresponding ENV var is set" do
-      ENV['FOO'] = nil
-      Env.foo(allow_nil: true).should be_nil
+      ENV['FOO_ALLOW_NIL'] = nil
+      Env.foo_allow_nil(:allow_nil => true).should be_nil
     end
 
   end
