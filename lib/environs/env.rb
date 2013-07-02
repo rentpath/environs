205b855241fe3cc78210cc3d1972217cb5d8038d
Colin Rymer <colin.rymer@gmail.com>
2013-07-02 12:03:16 -0400

    cleaning up the implementation

--- lib/environs/env.rb
+++ lib/environs/env.rb
@@ -3,16 +3,20 @@ require 'environs/missing_env_var_error'
 class Env
   class << self
     def method_missing(method, *args, &block)
-        key = method.to_s.upcase
-      if key.match(/\w+/)
-        ENV[key] || key_not_found_response(args.first, key)
-      else
-        super
-      end
+      key = method.to_s.upcase
+      key.match(/\w+/) ? env_key(key, args) : super
     end
 
-    def key_not_found_response(potential_hash, key)
-      if potential_hash.respond_to?(:fetch) && potential_hash.fetch(:allow_nil)
+    private
+
+    def env_key(key, nil_allowance)
+      value = ENV[key]
+      value.nil? ? key_not_found(nil_allowance, key) : value
+    end
+
+    def key_not_found(potential_hash, key)
+      hash = potential_hash.reduce({}, :merge)
+      if hash.fetch(:allow_nil) == true
         nil
       else
         raise MissingEnvVarError, "The #{key} environment variable is not set."
