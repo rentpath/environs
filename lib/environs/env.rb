950b6a966a2cc8fbe00240a4ac280629811430e1
Colin Rymer <colin.rymer@gmail.com>
2014-02-12 10:43:33 -0500

    simplify implementation

--- lib/environs/env.rb
+++ lib/environs/env.rb
@@ -4,23 +4,21 @@ class Env
   class << self
     def method_missing(method, *args, &block)
       key = method.to_s.upcase
-      key.match(/\w+/) ? env_key(key, args) : super
+      key.match(/\w+/) ? env_key(key, *args) : super
     end
 
     private
 
-    def env_key(key, nil_allowance)
-      value = ENV[key]
-      value.nil? ? key_not_found(nil_allowance, key) : value
+    def env_key(key, nil_allowance = {})
+      ENV.fetch(key) { |k| key_not_found(nil_allowance, key) }
     end
 
-    def key_not_found(potential_hash, key)
-      hash = potential_hash.reduce({}, :merge)
-      if hash.fetch(:allow_nil) == true
-        nil
-      else
-        raise MissingEnvVarError, "The #{key} environment variable is not set."
-      end
+    def key_not_found(nil_allowance, key)
+      nil_allowance[:allow_nil] ? nil : env_var_error(key)
+    end
+
+    def env_var_error(key)
+      raise MissingEnvVarError, "The #{key} environment variable is not set."
     end
   end
 end
